80de0ef7be5428b74158af2e60a386dfa54481a6
Suraj Upreti <surajupreti@Surajs-MacBook-Pro.local>
2017-02-28 16:40:43 -0500

    shows tweets on the homepage

---
+++ TwitterApp/TweetViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetViewCell.swift
+//  TwitterApp
+//
+//  Created by Suraj Upreti on 2/28/17.
+//  Copyright © 2017 Suraj Upreti. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
